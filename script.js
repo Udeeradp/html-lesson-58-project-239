4792b5aac9fa8f4a399c378466fc8b5b59f11bb1
Udeeradp <stu0480408@dpf.edu.lk>
2025-08-10 15:59:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,15 @@ let particles = [];
 let myColors = ["red", "green", "yellow", "blue"];
 
 function setup() {
-  let cnv = createCanvas(1200, 1200); 
+  let cnv = createCanvas(1200, 1200);
   cnv.position(300, 0, "fixed");
-  
-  // Create some particles
-  for (let i = 0; i < 100; i++) {
-    particles.push([random(width), random(height)]);
-  }
 }
 
-function createParticle() { 
+function createParticle() {
   for (let i = 0; i < particles.length; i++) {
-    let c = random(myColors); 
+    let c = random(myColors);
     fill(c);
-    noStroke(); 
+    noStroke();
     circle(particles[i][0], particles[i][1], 20);
   }
 }
@@ -23,4 +18,8 @@ function createParticle() {
 function draw() {
   background("#730201");
   createParticle();
+}
+
+function mouseDragged() {
+  particles.push([mouseX, mouseY]);
 }
